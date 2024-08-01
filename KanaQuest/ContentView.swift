90ff74c38a32e0b6fe21b3811a952fe23d8a78b7
Hannakaroline <karolinepalacios@gmail.com>
2024-08-01 14:06:47 -0400

    project iniciated]

---
+++ KanaQuest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KanaQuest
+//
+//  Created by Hanna on 01/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KanaQuest/KanaQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanaQuestApp.swift
+//  KanaQuest
+//
+//  Created by Hanna on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct KanaQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
